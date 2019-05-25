ae2c354afe232d536757c73bbe6894c20fdc5589
Nata <38878737+NataHanguga@users.noreply.github.com>
2019-05-26 02:45:30 +0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
 import { MainComponent } from './components/main/main.component';
 import { TableComponent } from './components/table/table.component';
-import { MenuComponent } from './components/menu/menu.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'list', component: ListComponent},
-  {path: 'table', component: TableComponent },
-  {path: 'menu', component: MenuComponent}
+  {path: 'table', component: TableComponent }
 ];
 
 @NgModule({
